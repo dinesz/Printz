1547b316536c69725bf457220d4365988638fc0e
dinesz <dineshksami@gmail.com>
2015-09-14 17:15:28 +0530

    Add files

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+     jQuery('.close-btn1').click(function(){
+      jQuery('#jumbo .header-text').html('<h1 class="header-text">We have<span> Give</span> Lot of Discounts</h3>');
+      jQuery('#jumbo .content').html('<p class="content">Give 20% discounts on every 1000 page Printouts</p>');
+});
+     jQuery('.close-btn2').click(function(){
+      jQuery('#jumbo .header-text').html('<h1 class="header-text">We serve<span> All document</span> Solutions</h1>');
+      jQuery('#jumbo .content').html('<p class="content">We have done bulk project documentation work for college students</p>');
+});
+     jQuery('.close-btn3').click(function(){
+      jQuery('#jumbo .header-text').html('<h1 class="header-text">We have <span>Hitech </span>Photo copier</23>');
+      jQuery('#jumbo .content').html('<p class="content">Initially We can provide high quality prints.</p>');
+});
+});
